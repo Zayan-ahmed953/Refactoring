a482c15f509581ecdd3e07af4445f5131f1a12cf
zayan_DevOps <zayan.ahmed@marsdevs.com>
2026-03-18 19:50:48 +0500

    Comented out dev BE tmp

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "dccc-prod-tfstate"
-    key    = "terraform/terraform.tfstate"
-    region = "us-gov-west-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "dccc-prod-tfstate"
+#     key    = "terraform/terraform.tfstate"
+#     region = "us-gov-west-1"
+#   }
+# }
