d2f06251778d26930f33180cb2254daec6e69049
zayan_DevOps <zayan.ahmed@marsdevs.com>
2026-03-26 12:20:35 +0500

    Added commit trigger for prod

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-02dfbd4ff395f2a1b"
-  instance_type = "t3.micro"
+  instance_type = "t3.nano"
   subnet_id = "subnet-07ccb110c0067e339"
 
   tags = {
