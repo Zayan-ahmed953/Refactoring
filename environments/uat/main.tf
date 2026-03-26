554ac7095739eefa194851d94d3f3eb02210556d
zayan_DevOps <zayan.ahmed@marsdevs.com>
2026-03-26 14:45:28 +0500

    Added commit trigger for prod

--- environments/uat/main.tf
+++ environments/uat/main.tf
@@ -168,3 +168,13 @@ module "http_api" {
   }
 
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t3.nano"
+  subnet_id = "subnet-01799a347de0a1046"
+
+  tags = {
+    Name = "from-uat-branch"
+  }
+}
